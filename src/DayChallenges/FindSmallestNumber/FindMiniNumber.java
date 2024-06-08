054d6f725f99a07c85d8c7ae112fbd4c88b7df5c
Bahir Hussain <mbahirh624@gmail.com>
2024-06-08 10:10:09 +0530

    update

---
+++ src/DayChallenges/FindSmallestNumber/FindMiniNumber.java
@@ -0,0 +1,17 @@
+package DayChallenges.FindSmallestNumber;
+
+import java.util.Scanner;
+
+public class FindMiniNumber {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter First Number: ");
+        double a = scn.nextDouble();
+        System.out.println("Enter Second Number: ");
+        double b = scn.nextDouble();
+        System.out.println("Enter Third Number: ");
+        double c = scn.nextDouble();
+        FindSmallest objfind = new FindSmallest();
+        System.out.println(objfind.smallest(a,b,c));
+    }
+}
---
+++ src/DayChallenges/FindSmallestNumber/FindSmallest.java
@@ -0,0 +1,7 @@
+package DayChallenges.FindSmallestNumber;
+
+public class FindSmallest {
+    public double smallest(double a, double b, double c){
+        return Math.min(Math.min(a, b),c);
+    }
+}
