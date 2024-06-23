5c4d95968b59818fe0e00e4d9ff152c741dee315
Bahir Hussain <mbahirh624@gmail.com>
2024-06-23 19:18:47 +0530

    Update

--- src/ExceptionHandlingJava/DivideNumbers.java
+++ src/ExceptionHandlingJava/DivideNumbers.java
@@ -3,14 +3,14 @@ package ExceptionHandlingJava;
 public class DivideNumbers {
     public static void main(String[] args){
         try {
-            int result = divideNumber(9,2);
+            int result = divideNumber(2,0);
             System.out.println("Result: " + result);
         } catch (ArithmeticException e){
             System.out.println("Error: " + e.getMessage());
         }
     }
     public static int divideNumber(int dividend, int divisor){
-        if(divisor ==0) {
+        if(divisor == 0) {
             throw new ArithmeticException("Cannot divide the given number by zero!");
         }
         return dividend / divisor;
